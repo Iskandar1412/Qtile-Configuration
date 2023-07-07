f34464dfecc288bde3e3b72c9d68446c989a3d93
iskandar <silvapaco810@gmail.com>
2023-07-07 21:56:46 +0000

    V1

---
+++ autostart.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Configuracion Teclado
+setxkbmap latam &
+
+# Configuracion pantalla
+xrandr --output eDP1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output HDMI1 --off --output VIRTUAL1 --off &
+
+# Iconos del sistema
+
+udiskie -t &
+
+nm-applet &
+
+volumeicon &
+
+cbatticon -u 5 &
+
+# Fondo de escritorio
+nitrogen --restore &
